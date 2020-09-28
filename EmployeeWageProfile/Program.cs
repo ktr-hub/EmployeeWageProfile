411d1099018c6d92a6dbc39b28b130c8bb6fc925
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 13:39:00 +0530

    [Tirupathi] Add. Welcome Portal in master

---
+++ EmployeeWageProfile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages Portal!");
+        }
+    }
+}
