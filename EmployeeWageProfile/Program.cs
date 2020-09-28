4266b4d649c8e4f862d786a07f1d4af053358db2
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 14:23:25 +0530

    [Tirupathi] Refactor. Removed 2 unnecessary variables in UC1

--- EmployeeWageProfile/Program.cs
+++ EmployeeWageProfile/Program.cs
@@ -6,12 +6,8 @@ namespace EmployeeWageProfile
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
             Random random = new Random();
-
             int empCheck = random.Next(0, 2);
-
-            Console.WriteLine(empCheck);
 
             if (empCheck == IS_FULL_TIME)
                 Console.WriteLine("Employee is present");
