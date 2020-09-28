61de88c2b2fb9040996c047def223a7e3e9dedfa
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 14:21:14 +0530

    [Tirupathi] Add. UC1- Checks whether Employee present or not

--- EmployeeWageProfile/Program.cs
+++ EmployeeWageProfile/Program.cs
@@ -6,7 +6,17 @@ namespace EmployeeWageProfile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wages Portal!");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            Console.WriteLine(empCheck);
+
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is not present");
         }
     }
 }
